32dd1d384b10e5bfbc9b156480dfa32bfeaa039f
Cecile Veneziani <contact@cecilitse.org>
2019-12-01 21:48:27 +0100

    Add a first profile to the demo web app

--- demo-web-app/app.rb
+++ demo-web-app/app.rb
@@ -6,6 +6,13 @@ enable :static
 
 get "/" do
   @profiles = [
+    {
+      "color"        => "yellow",
+      "first_name"   => "Cecile",
+      "quote"        => "All you need is code",
+      "picture_url"  => "https://www.cecilitse.org/images/avatar-db7c2e53.png",
+      "website_url"  => "https://www.cecilitse.org/"
+    }
     # TODO: gather a few profiles
   ]
 
