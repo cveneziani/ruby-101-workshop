4bc45f3d02506ad2adc3c220ca8dd018db99aae1
Cecile Veneziani <contact@cecilitse.org>
2019-12-01 23:31:15 +0100

    Add a custom color per profile to demo web app

--- demo-web-app/app.rb
+++ demo-web-app/app.rb
@@ -4,10 +4,18 @@ require "sinatra/reloader" if development?
 
 enable :static
 
+# Available colors
+BLUE   = "blue"
+GREEN  = "green"
+PURPLE = "purple"
+YELLOW = "yellow"
+
+COLORS = [BLUE, GREEN, PURPLE, YELLOW]
+
 get "/" do
   @profiles = [
     {
-      "color"        => "yellow",
+      "color"        => YELLOW,
       "first_name"   => "Cecile",
       "quote"        => "All you need is code",
       "picture_url"  => "https://www.cecilitse.org/images/avatar-db7c2e53.png",
